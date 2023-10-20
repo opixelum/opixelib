aa94f99e376afba16c1f63d778267b16156860b7
opixelum <48217573+opixelum@users.noreply.github.com>
2023-10-20 15:35:04 +0200

    Declare `comb_sort()` in `sorting.h`

--- c/include/sorting.h
+++ c/include/sorting.h
@@ -20,4 +20,12 @@ void insertion_sort(int *array, size_t size);
  */
 void odd_even_sort(int *array, size_t size);
 
+/**
+ * @brief Complexity: O(n^2)
+ * @param array Pointer to the array to be sorted
+ * @param size Size of the array
+ * @warning Modifies the array
+ */
+void comb_sort(int *array, size_t size);
+
 #endif // SORTING_H
