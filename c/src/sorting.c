a1560be7ff2df92630f845dee9b47123474cf678
opixelum <48217573+opixelum@users.noreply.github.com>
2023-10-20 15:26:33 +0200

    Define `odd_even_sort()` in `sorting.c`

--- c/src/sorting.c
+++ c/src/sorting.c
@@ -14,3 +14,33 @@ void insertion_sort(int *array, size_t size)
         array[j+1] = key;
     }
 }
+
+void odd_even_sort(int *array, size_t size)
+{
+    int sorted = 0;
+
+    while (!sorted)
+    {
+        sorted = 1;
+
+        // Even-odd phase
+        for (size_t i = 0; i < size - 1; i += 2)
+        {
+            if (array[i] > array[i + 1])
+            {
+                swap(&array[i], &array[i + 1], sizeof(int));
+                sorted = -1;
+            }
+        }
+
+        // Odd-even phase
+        for (size_t i = 1; i < size - 1; i += 2)
+        {
+            if (array[i] > array[i + 1])
+            {
+                swap(&array[i], &array[i + 1], sizeof(int));
+                sorted = 0;
+            }
+        }
+    }
+}
