1678802dc705bdab3cb094f6d98adf04c0cb8f60
opixelum <48217573+opixelum@users.noreply.github.com>
2023-10-20 15:15:59 +0200

    Add `swap()` for any type

--- c/include/utils.h
+++ c/include/utils.h
@@ -1,4 +1,15 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * @brief Swaps two variables of any type
+ * @param a Pointer to the first variable
+ * @param b Pointer to the second variable
+ * @param size Size of the variables
+ */
+void swap(void *a, void *b, size_t size);
+
 #endif // UTILS_H
--- c/src/utils.c
+++ c/src/utils.c
@@ -1,1 +1,9 @@
 #include "utils.h"
+
+void swap(void *a, void *b, size_t size)
+{
+    char temp[size];
+    memcpy(temp, a, size);
+    memcpy(a, b, size);
+    memcpy(b, temp, size);
+}
