4aa305453227829e99df7f5cd8d86f24fdd54d9e
opixelum <48217573+opixelum@users.noreply.github.com>
2023-10-20 15:01:48 +0200

    Declare `odd_even_sort()` in `sorting.h`

--- c/include/sorting.h
+++ c/include/sorting.h
@@ -11,4 +11,12 @@
  */
 void insertion_sort(int *array, size_t size);
 
+/**
+ * @brief Complexity: O(n^2)
+ * @param array Pointer to the array to be sorted
+ * @param size Size of the array
+ * @warning Modifies the array
+ */
+void odd_even_sort(int *array, size_t size);
+
 #endif // SORTING_H
