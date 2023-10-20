30decb84c9ba448e34e49d1fd6880f6a6b6b190a
opixelum <48217573+opixelum@users.noreply.github.com>
2023-10-20 15:00:08 +0200

    Define `insertion_sort()`

--- c/src/sorting.c
+++ c/src/sorting.c
@@ -1,1 +1,16 @@
 #include "sorting.h"
+
+void insertion_sort(int *array, size_t size)
+{
+    for (size_t i = 1; i < size; i++)
+    {
+        int key = array[i];
+        int j = i - 1;
+        while (j >= 0 && array[j] > key)
+        {
+            array[j+1] = array[j];
+            j--;
+        }
+        array[j+1] = key;
+    }
+}
