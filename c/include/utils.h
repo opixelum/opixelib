d4a42b1be1891dd26dfd426145c2cfb749bb386b
opixelum <48217573+opixelum@users.noreply.github.com>
2023-10-24 11:21:26 +0200

    Make full sentences in doxygen for `utils.h`

--- c/include/utils.h
+++ c/include/utils.h
@@ -5,10 +5,11 @@
 #include <string.h>
 
 /**
- * @brief Swaps two variables of any type
- * @param a Pointer to the first variable
- * @param b Pointer to the second variable
- * @param size Size of the variables
+ * @brief Swaps two variables of any type.
+ * @param a A pointer to the first variable.
+ * @param b A pointer to the second variable.
+ * @param size The size of the variables.
+ * @warning Both variables need to be of the same type.
  */
 void swap(void *a, void *b, size_t size);
 
