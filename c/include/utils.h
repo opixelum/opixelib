2a4791d8f6d60cc52532f80411612a514360004b
opixelum <48217573+opixelum@users.noreply.github.com>
2023-10-24 11:40:29 +0200

    Add `merge_sorted_array()` in `utils`

--- c/include/utils.h
+++ c/include/utils.h
@@ -13,4 +13,21 @@
  */
 void swap(void *a, void *b, size_t size);
 
+/**
+ * @brief Merges two sorted arrays into one sorted array.
+ * @param array_a A pointer to the first array to merge.
+ * @param size_a The size of the first array.
+ * @param array_b A pointer to the second array to merge.
+ * @param size_b The size of the second array.
+ * @return A pointer to the sorted array.
+ * @warning This function doesn't modify passed arrays, but creates another one.
+ */
+int *merge_sorted_array
+(
+    int *array_a,
+    size_t size_a,
+    int *array_b,
+    size_t size_b
+);
+
 #endif // UTILS_H
--- c/src/utils.c
+++ c/src/utils.c
@@ -7,3 +7,29 @@ void swap(void *a, void *b, size_t size)
     memcpy(a, b, size);
     memcpy(b, temp, size);
 }
+
+int *merge_sorted_array
+(
+    int *array_a,
+    size_t size_a,
+    int *array_b,
+    size_t size_b
+) {
+    int *merge_array = malloc((size_a + size_b) * sizeof *merge_array);
+    size_t i = 0; // Tracks array_a index
+    size_t j = 0; // Tracks array_b index
+    size_t k = 0; // Tracks merged_array index
+
+    // Merge arrays by picking the smallest element until one is empty
+    while (i < size_a && j < size_b)
+    {
+        if (array_a[i] < array_b[j]) merge_array[k++] = array_a[i++];
+        else merge_array[k++] = array_b[j++];
+    }
+
+    // Copy remaining elements of array_a or array_b
+    while (i < size_a) merge_array[k++] = array_a[i++];
+    while (j < size_b) merge_array[k++] = array_b[j++];
+
+    return merge_array;
+}
