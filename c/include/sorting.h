c308ce4fc9b51f0622505c96b16fa7a9d8d124ee
opixelum <48217573+opixelum@users.noreply.github.com>
2023-10-24 11:14:13 +0200

    Improve doxygen

--- c/include/sorting.h
+++ c/include/sorting.h
@@ -5,34 +5,39 @@
 #include "utils.h"
 
 /**
- * @brief Complexity: O(n^2)
+ * @brief Simple sorting algorithm that builds the final sorted array one item
+ * at a time by repeatedly picking one element and placing it at the correct
+ * position. Complexity: O(n^2)
  * @param array Pointer to the array to be sorted
  * @param size Size of the array
- * @warning Modifies the array
+ * @warning This function modifies the array
  */
 void insertion_sort(int *array, size_t size);
 
 /**
- * @brief Complexity: O(n^2)
+ * @brief Variation of bubble sort where comparisons and swaps are performed on
+ * alternating odd and even indexed elements. Complexity: O(n^2)
  * @param array Pointer to the array to be sorted
  * @param size Size of the array
- * @warning Modifies the array
+ * @warning This function modifies the array
  */
 void odd_even_sort(int *array, size_t size);
 
 /**
- * @brief Complexity: O(n^2)
+ * @brief Improved bubble sort that uses a gap sequence to remove turtles (small
+ * values near the end). Complexity: O(n^2)
  * @param array Pointer to the array to be sorted
  * @param size Size of the array
- * @warning Modifies the array
+ * @warning This function modifies the array
  */
 void comb_sort(int *array, size_t size);
 
 /**
- * @brief Complexity: O(n^2)
+ * @brief Optimization of bubble sort that sorts in both directions on each pass
+ * through the list. Complexity: O(n^2)
  * @param array Pointer to the array to be sorted
  * @param size Size of the array
- * @warning Modifies the array
+ * @warning This function modifies the array
  */
 void cocktail_sort(int *array, size_t size);
 
