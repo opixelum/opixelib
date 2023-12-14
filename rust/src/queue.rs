eeb57192d6a3e87706f96d6b66d735eac85b0b47
opixelum <48217573+opixelum@users.noreply.github.com>
2023-12-14 12:58:27 +0100

    Add `is_full` function for queue in Rust

--- rust/src/queue.rs
+++ rust/src/queue.rs
@@ -9,6 +9,10 @@ impl<T> Queue<T> {
     pub fn is_empty(&self) -> bool {
         self.size == 0
     }
+
+    pub fn is_full(&self) -> bool {
+        self.size == self.capacity
+    }
 }
 
 #[cfg(test)]
@@ -23,16 +27,22 @@ mod tests {
         capacity: 1
     };
 
-    static INT_QUEUE: Lazy<Queue<i8>> = Lazy::new(|| Queue {
-        items: vec![1, 2, 3, 4],
+    static FULL_INT_QUEUE: Lazy<Queue<i8>> = Lazy::new(|| Queue {
+        items: vec![0],
         start: 0,
-        size: 4,
-        capacity: 5
+        size: 1,
+        capacity: 1
     });
 
     #[test]
     fn test_is_empty() {
         assert_eq!(EMPTY_QUEUE.is_empty(), true);
-        assert_eq!(INT_QUEUE.is_empty(), false);
+        assert_eq!(FULL_INT_QUEUE.is_empty(), false);
+    }
+
+    #[test]
+    fn test_is_full() {
+        assert_eq!(EMPTY_QUEUE.is_full(), false);
+        assert_eq!(FULL_INT_QUEUE.is_full(), true);
     }
 }
