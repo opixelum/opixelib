2047140fe982f696641622f0c3e6c779d7680aec
opixelum <48217573+opixelum@users.noreply.github.com>
2023-12-15 12:07:27 +0100

    Add `enqueue` function for queue in Rust

--- rust/src/queue.rs
+++ rust/src/queue.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, PartialEq)]
 pub struct Queue<T> {
     items: Vec<T>,
     start: usize,
@@ -12,6 +13,16 @@ impl<T> Queue<T> {
 
     pub fn is_full(&self) -> bool {
         self.size == self.capacity
+    }
+
+    pub fn enqueue(&mut self, value: T) -> Result<usize, &str> {
+        if self.is_full() {Err("Queue is full.")}
+        else {
+            let index = (self.start + self.size) % self.capacity;
+            self.items[index] = value;
+            self.size += 1;
+            return Ok(index);
+        }
     }
 }
 
@@ -28,7 +39,7 @@ mod tests {
     };
 
     static FULL_INT_QUEUE: Lazy<Queue<i8>> = Lazy::new(|| Queue {
-        items: vec![0],
+        items: vec![1],
         start: 0,
         size: 1,
         capacity: 1,
@@ -44,5 +55,38 @@ mod tests {
     fn test_is_full() {
         assert_eq!(EMPTY_QUEUE.is_full(), false);
         assert_eq!(FULL_INT_QUEUE.is_full(), true);
+    }
+
+    #[test]
+    fn test_enqueue() {
+        let mut queue = Queue {
+            items: vec![0],
+            start: 0,
+            size: 0,
+            capacity: 1,
+        };
+        let expected_result = Queue {
+            items: vec![1],
+            start: 0,
+            size: 1,
+            capacity: 1,
+        };
+        assert_eq!(queue.enqueue(1), Ok(0));
+        assert_eq!(queue, expected_result);
+
+        let mut queue = Queue {
+            items: vec![0, 1],
+            start: 1,
+            size: 1,
+            capacity: 2,
+        };
+        let expected_result = Queue {
+            items: vec![1, 1],
+            start: 1,
+            size: 2,
+            capacity: 2,
+        };
+        assert_eq!(queue.enqueue(1), Ok(0));
+        assert_eq!(queue, expected_result);
     }
 }
