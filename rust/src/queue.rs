785693b7e454a61e056c0eaed8e6e5b8b57dee1e
opixelum <48217573+opixelum@users.noreply.github.com>
2023-12-15 12:37:34 +0100

    Add dequeuing in Rust

--- rust/src/queue.rs
+++ rust/src/queue.rs
@@ -16,12 +16,22 @@ impl<T> Queue<T> {
     }
 
     pub fn enqueue(&mut self, value: T) -> Result<usize, &str> {
-        if self.is_full() {Err("Queue is full")}
+        if self.is_full() {Err("Unable to enqueue a full queue")}
         else {
             let index = (self.start + self.size) % self.capacity;
             self.items[index] = value;
             self.size += 1;
             return Ok(index);
+        }
+    }
+
+    pub fn dequeue(&mut self) -> Result<&T, &str> {
+        if self.is_empty() {Err("Unable to dequeue an empty queue")}
+        else {
+            let dequeued_item = &self.items[self.start];
+            self.start = (self.start + 1) % self.capacity;
+            self.size -= 1;
+            return Ok(dequeued_item);
         }
     }
 }
@@ -60,35 +70,38 @@ mod tests {
     #[test]
     fn test_enqueue() {
         let mut queue = Queue {
+            items: vec![0, 0],
+            start: 0,
+            size: 1,
+            capacity: 2,
+        };
+        let expected_result = Queue {
+            items: vec![0, 1],
+            start: 0,
+            size: 2,
+            capacity: 2,
+        };
+        assert_eq!(queue.enqueue(1), Ok(1));
+        assert_eq!(queue, expected_result);
+        assert_eq!(queue.enqueue(1), Err("Unable to enqueue a full queue"));
+    }
+
+    #[test]
+    fn test_dequeue() {
+        let mut queue = Queue {
+            items: vec![0],
+            start: 0,
+            size: 1,
+            capacity: 1,
+        };
+        let expected_result = Queue {
             items: vec![0],
             start: 0,
             size: 0,
             capacity: 1,
         };
-        let expected_result = Queue {
-            items: vec![1],
-            start: 0,
-            size: 1,
-            capacity: 1,
-        };
-        assert_eq!(queue.enqueue(1), Ok(0));
+        assert_eq!(queue.dequeue(), Ok(&0));
         assert_eq!(queue, expected_result);
-
-        let mut queue = Queue {
-            items: vec![0, 1],
-            start: 1,
-            size: 1,
-            capacity: 2,
-        };
-        let expected_result = Queue {
-            items: vec![1, 1],
-            start: 1,
-            size: 2,
-            capacity: 2,
-        };
-        assert_eq!(queue.enqueue(1), Ok(0));
-        assert_eq!(queue, expected_result);
-
-        assert_eq!(queue.enqueue(1), Err("Queue is full"));
+        assert_eq!(queue.dequeue(), Err("Unable to dequeue an empty queue"));
     }
 }
