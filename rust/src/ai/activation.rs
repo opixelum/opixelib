debe4973bbd52c8e3a453e88dce6bdf50371e43e
opixelum <48217573+opixelum@users.noreply.github.com>
2024-01-06 02:14:54 +0100

    Add leaky ReLU

--- rust/src/ai/activation.rs
+++ rust/src/ai/activation.rs
@@ -3,6 +3,11 @@ pub fn relu(x: f64) -> f64 {
     else {0.0}
 }
 
+pub fn leaky_relu(x: f64) -> f64 {
+    if x > 0.0 {x}
+    else {0.01 * x}
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -12,5 +17,12 @@ mod tests {
         assert_eq!(relu(-1.0), 0.0);
         assert_eq!(relu(0.0), 0.0);
         assert_eq!(relu(1.0), 1.0);
+    }
+
+    #[test]
+    fn test_leaky_relu() {
+        assert_eq!(leaky_relu(-1.0), -0.01);
+        assert_eq!(leaky_relu(0.0), 0.0);
+        assert_eq!(leaky_relu(1.0), 1.0);
     }
 }
