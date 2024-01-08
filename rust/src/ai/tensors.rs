748c44b753bc25592e77d8056823f7af51a7a089
opixelum <48217573+opixelum@users.noreply.github.com>
2024-01-08 19:15:28 +0100

    Add set methods tests for tensors

--- rust/src/ai/tensors.rs
+++ rust/src/ai/tensors.rs
@@ -86,9 +86,39 @@ where
 
     fn get(&self, depth: usize, row: usize, column: usize) -> Option<&T> {
         if depth < self.depth && row < self.rows && column < self.columns {
-            self.data.get(depth * self.rows * self.columns + row * self.columns + column)
+            self.data
+                .get(depth * self.rows * self.columns + row * self.columns + column)
         } else {
             None
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new_tensor1d() {
+        let tensor: Tensor1D<u8> = Tensor1D::new(2);
+        assert_eq!(tensor.data, vec![0, 0]);
+        assert_eq!(tensor.length, 2);
+    }
+
+    #[test]
+    fn test_new_tensor2d() {
+        let tensor: Tensor2D<u8> = Tensor2D::new(2, 2);
+        assert_eq!(tensor.data, vec![0, 0, 0, 0]);
+        assert_eq!(tensor.rows, 2);
+        assert_eq!(tensor.columns, 2);
+    }
+
+    #[test]
+    fn test_new_tensor3d() {
+        let tensor: Tensor3D<u8> = Tensor3D::new(2, 2, 2);
+        assert_eq!(tensor.data, vec![0, 0, 0, 0, 0, 0, 0, 0]);
+        assert_eq!(tensor.depth, 2);
+        assert_eq!(tensor.rows, 2);
+        assert_eq!(tensor.columns, 2);
     }
 }
