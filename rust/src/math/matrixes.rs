b152cff8ad943a3590c71ed07eda6fc5d95b2c46
opixelum <48217573+opixelum@users.noreply.github.com>
2024-01-26 20:38:30 +0100

    use matrix struct in existing math functions

--- rust/src/math/matrixes.rs
+++ rust/src/math/matrixes.rs
@@ -46,34 +46,42 @@ where
     }
 }
 
-pub fn pairwise_operation<T, F>(a: &[T], b: &[T], mut operator: F) -> Vec<Vec<T>>
+pub fn pairwise_operation<T, F>(a: &[T], b: &[T], mut operator: F) -> Matrix<T>
 where
     T: Clone + Default,
     F: FnMut(&T, &T) -> T,
 {
     let mut result = vec![vec![T::default(); b.len()]; a.len()];
+
     for (i, a_elem) in a.iter().enumerate() {
         for (j, b_elem) in b.iter().enumerate() {
             result[i][j] = operator(a_elem, b_elem);
         }
     }
-    result
+
+    Matrix {
+        data: result.into_iter().flatten().collect(),
+        shape: vec![a.len(), b.len()],
+    }
 }
 
-pub fn diagonal_operation<T, F>(matrix: &[Vec<T>], mut operator: F) -> Vec<T>
+pub fn diagonal_operation<T, F>(matrix: &Matrix<T>, mut operator: F) -> Vec<T>
 where
     T: Clone + Default,
     F: FnMut(T, T) -> T,
 {
-    let rows = matrix.len();
-    let cols = if rows > 0 { matrix[0].len() } else { 0 };
+    let rows = matrix.shape[0];
+    let cols = if rows > 0 { matrix.shape[1] } else { 0 };
     let mut result = vec![T::default(); rows + cols - 1];
 
     for i in 0..rows {
         for j in 0..cols {
             let result_index = i + j;
             if result_index < result.len() {
-                result[result_index] = operator(result[result_index].clone(), matrix[i][j].clone());
+                result[result_index] = operator(
+                    result[result_index].clone(),
+                    matrix.get(&[i, j]).unwrap().clone(),
+                );
             }
         }
     }
