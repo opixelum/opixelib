301aae78960fd0fa97abd3b43cf4ed0398aa27e5
opixelum <48217573+opixelum@users.noreply.github.com>
2024-02-06 18:29:39 +0100

    Separate queue tests

--- rust/src/queue.rs
+++ rust/src/queue.rs
@@ -1,9 +1,9 @@
 #[derive(Debug, PartialEq)]
 pub struct Queue<T> {
-    items: Vec<T>,
-    start: usize,
-    size: usize,
-    capacity: usize,
+    pub items: Vec<T>,
+    pub start: usize,
+    pub size: usize,
+    pub capacity: usize,
 }
 
 impl<T> Queue<T> {
@@ -22,7 +22,7 @@ impl<T> Queue<T> {
             let index = (self.start + self.size) % self.capacity;
             self.items[index] = value;
             self.size += 1;
-            return Ok(index);
+            Ok(index)
         }
     }
 
@@ -33,77 +33,7 @@ impl<T> Queue<T> {
             let dequeued_item = &self.items[self.start];
             self.start = (self.start + 1) % self.capacity;
             self.size -= 1;
-            return Ok(dequeued_item);
+            Ok(dequeued_item)
         }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use once_cell::sync::Lazy;
-
-    static EMPTY_QUEUE: Queue<i8> = Queue {
-        items: Vec::new(),
-        start: 0,
-        size: 0,
-        capacity: 1,
-    };
-
-    static FULL_INT_QUEUE: Lazy<Queue<i8>> = Lazy::new(|| Queue {
-        items: vec![1],
-        start: 0,
-        size: 1,
-        capacity: 1,
-    });
-
-    #[test]
-    fn test_is_empty() {
-        assert_eq!(EMPTY_QUEUE.is_empty(), true);
-        assert_eq!(FULL_INT_QUEUE.is_empty(), false);
-    }
-
-    #[test]
-    fn test_is_full() {
-        assert_eq!(EMPTY_QUEUE.is_full(), false);
-        assert_eq!(FULL_INT_QUEUE.is_full(), true);
-    }
-
-    #[test]
-    fn test_enqueue() {
-        let mut queue = Queue {
-            items: vec![0, 0],
-            start: 0,
-            size: 1,
-            capacity: 2,
-        };
-        let expected_result = Queue {
-            items: vec![0, 1],
-            start: 0,
-            size: 2,
-            capacity: 2,
-        };
-        assert_eq!(queue.enqueue(1), Ok(1));
-        assert_eq!(queue, expected_result);
-        assert_eq!(queue.enqueue(1), Err("Unable to enqueue a full queue"));
-    }
-
-    #[test]
-    fn test_dequeue() {
-        let mut queue = Queue {
-            items: vec![0],
-            start: 0,
-            size: 1,
-            capacity: 1,
-        };
-        let expected_result = Queue {
-            items: vec![0],
-            start: 0,
-            size: 0,
-            capacity: 1,
-        };
-        assert_eq!(queue.dequeue(), Ok(&0));
-        assert_eq!(queue, expected_result);
-        assert_eq!(queue.dequeue(), Err("Unable to dequeue an empty queue"));
     }
 }
---
+++ rust/tests/queue.rs
@@ -0,0 +1,66 @@
+use once_cell::sync::Lazy;
+use opixelib::queue::*;
+
+static EMPTY_QUEUE: Queue<i8> = Queue {
+    items: Vec::new(),
+    start: 0,
+    size: 0,
+    capacity: 1,
+};
+
+static FULL_INT_QUEUE: Lazy<Queue<i8>> = Lazy::new(|| Queue {
+    items: vec![1],
+    start: 0,
+    size: 1,
+    capacity: 1,
+});
+
+#[test]
+fn test_is_empty() {
+    assert_eq!(EMPTY_QUEUE.is_empty(), true);
+    assert_eq!(FULL_INT_QUEUE.is_empty(), false);
+}
+
+#[test]
+fn test_is_full() {
+    assert_eq!(EMPTY_QUEUE.is_full(), false);
+    assert_eq!(FULL_INT_QUEUE.is_full(), true);
+}
+
+#[test]
+fn test_enqueue() {
+    let mut queue = Queue {
+        items: vec![0, 0],
+        start: 0,
+        size: 1,
+        capacity: 2,
+    };
+    let expected_result = Queue {
+        items: vec![0, 1],
+        start: 0,
+        size: 2,
+        capacity: 2,
+    };
+    assert_eq!(queue.enqueue(1), Ok(1));
+    assert_eq!(queue, expected_result);
+    assert_eq!(queue.enqueue(1), Err("Unable to enqueue a full queue"));
+}
+
+#[test]
+fn test_dequeue() {
+    let mut queue = Queue {
+        items: vec![0],
+        start: 0,
+        size: 1,
+        capacity: 1,
+    };
+    let expected_result = Queue {
+        items: vec![0],
+        start: 0,
+        size: 0,
+        capacity: 1,
+    };
+    assert_eq!(queue.dequeue(), Ok(&0));
+    assert_eq!(queue, expected_result);
+    assert_eq!(queue.dequeue(), Err("Unable to dequeue an empty queue"));
+}
