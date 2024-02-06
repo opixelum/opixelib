ff1ff9bc1941a66b45727163eea5b206bdee9e93
opixelum <48217573+opixelum@users.noreply.github.com>
2024-02-06 18:16:31 +0100

    Separate activations tests

---
+++ rust/src/ai/activations.rs
@@ -0,0 +1,23 @@
+pub fn relu(x: f64) -> f64 {
+    if x > 0.0 {
+        x
+    } else {
+        0.0
+    }
+}
+
+pub fn leaky_relu(x: f64) -> f64 {
+    if x > 0.0 {
+        x
+    } else {
+        0.01 * x
+    }
+}
+
+pub fn softmax(values: &[f64]) -> Vec<f64> {
+    let max_val = values.iter().fold(f64::NEG_INFINITY, |a, &b| a.max(b));
+    let exps: Vec<f64> = values.iter().map(|&x| (x - max_val).exp()).collect();
+    let sum_exps: f64 = exps.iter().sum();
+
+    exps.iter().map(|&exp_val| exp_val / sum_exps).collect()
+}
--- rust/src/ai/mod.rs
+++ rust/src/ai/mod.rs
@@ -1,4 +1,4 @@
-pub mod activation;
+pub mod activations;
 pub mod data;
 pub mod layers;
 pub mod losses;
