793608fe9561b0b22b5b19365cb0b142642b7378
Opixelum <48217573+opixelum@users.noreply.github.com>
2024-03-06 19:07:02 +0100

    Add bias to neuron

--- rust/src/ai/neuron.rs
+++ rust/src/ai/neuron.rs
@@ -3,10 +3,11 @@ use crate::math::vector::dot;
 pub struct Neuron {
     pub inputs: Vec<f64>,
     pub weights: Vec<f64>,
+    pub bias: f64,
 }
 
-pub fn forward(neuron: Neuron) -> Result<f64, &'static str> {
-    dot(&neuron.inputs, &neuron.weights)
+pub fn forward(neuron: Neuron) -> f64 {
+    dot(&neuron.inputs, &neuron.weights).unwrap() + neuron.bias
 }
 
 #[cfg(test)]
@@ -18,7 +19,8 @@ mod tests {
         let neuron: Neuron = Neuron {
             inputs: vec![1.0, 2.0, 3.0],
             weights: vec![4.0, 5.0, 6.0],
+            bias: 10.0,
         };
-        assert_eq!(forward(neuron), Ok(32.0))
+        assert_eq!(forward(neuron), 42.0)
     }
 }
