9d5ab04fedc204ce907e5b1d8a5ae5f6d486736e
Opixelum <48217573+opixelum@users.noreply.github.com>
2024-03-06 19:44:10 +0100

    Move neuron tests in separate file

--- rust/src/ai/neuron.rs
+++ rust/src/ai/neuron.rs
@@ -11,18 +11,3 @@ impl Neuron {
         dot(&self.inputs, &self.weights).unwrap() + self.bias
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_forward() {
-        let neuron: Neuron = Neuron {
-            inputs: vec![1.0, 2.0, 3.0],
-            weights: vec![4.0, 5.0, 6.0],
-            bias: 10.0,
-        };
-        assert_eq!(neuron.forward(), 42.0)
-    }
-}
