eb8a2f2c2cd83149d81b0717d0871460b2e1a438
Opixelum <48217573+opixelum@users.noreply.github.com>
2024-03-19 00:10:35 +0100

    Change "size" to "length"

--- rust/src/math/vector.rs
+++ rust/src/math/vector.rs
@@ -1,37 +1,38 @@
 use std::iter::Sum;
 use std::ops::{Add, Mul, Sub};
 
-/// Sums two vectors of the same size, returning an error if sizes differ.
+/// Sums two vectors of the same length, returning an error if lengths differ.
 pub fn sum<T>(a: &[T], b: &[T]) -> Result<Vec<T>, &'static str>
 where
     T: Add<Output = T> + Copy,
 {
     if a.len() != b.len() {
-        Err("Vectors must be of the same size.")
+        Err("Vectors must be of the same length.")
     } else {
         Ok(a.iter().zip(b.iter()).map(|(a, b)| *a + *b).collect())
     }
 }
 
-/// Calculates the dot product of two vectors of the same size, returning an error if sizes differ.
+/// Calculates the dot product of two vectors of the same length, returning an error if lengths differ.
 pub fn dot<T>(a: Vec<T>, b: Vec<T>) -> Result<T, &'static str>
 where
     T: Mul<Output = T> + Add<Output = T> + Sum<T> + Copy,
 {
     if a.len() != b.len() {
-        Err("Vectors must be of the same size.")
+        Err("Vectors must be of the same length.")
     } else {
         Ok(a.iter().zip(b.iter()).map(|(x, y)| *x * *y).sum())
     }
 }
 
+// Elementwise substraction of two vectors of the same length, returning an error if lengths differ.
 pub fn sub<T>(a: Vec<T>, b: Vec<T>) -> Result<Vec<T>, &'static str>
 where
     T: Sub,
     Vec<T>: FromIterator<<T as Sub>::Output>,
 {
     if a.len() != b.len() {
-        Err("Vectors must be of the same size.")
+        Err("Vectors must be of the same length.")
     } else {
         Ok(a.into_iter().zip(b).map(|(a, b)| a - b).collect())
     }
